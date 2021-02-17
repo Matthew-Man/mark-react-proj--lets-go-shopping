ce24694bf9566fcecb82d93a8590408a5b9fb78e
Muhammad Tanvir Bin Ali <ali2912@outlook.com>
2021-02-17 16:17:49 +0000

    finished handleSubmit function

--- src/app.tsx
+++ src/app.tsx
@@ -18,32 +18,37 @@ function App() {
 
 
     function handleSubmit() {
-        shoppingList.push({nameOfItem: itemName, amount: quantity, amountUnits: quantityUnits});
+        shoppingList.push({ nameOfItem: itemName, amount: quantity, amountUnits: quantityUnits });
+        setItemName("")
+        setQuantity("0")
+        setQuantityUnits("kilograms")
+        console.log(shoppingList)
     }
-    
-    
+
+
     return (
         <div>
-            <form id="shopping-list" onSubmit={handleSubmit}>
-                <label>
-                    Item: 
-                    <input type="text" id="name" onChange={(e) => setItemName(e.target.value)}/>
-                </label>
-                <br /><br />
-                <label>
-                    Quantity: 
-                    <input type="number" id="quantity" onChange={(e) => setQuantity(e.target.value)}/>
-                </label>
-                <select name="quantity-units" id="units" onChange={(e) => setQuantityUnits(e.target.value)}>
-                    <option value="kilograms">Kilograms</option>
-                    <option value="grams">Grams</option>
-                    <option value="litres">Litres</option>
-                    <option value="number">Number</option>
-                </select>
-                <br/>
-                <p>Click the submit button to add to shopping list.</p>
-                <input type="submit" value="Submit"/>
-            </form>
+            {/* <form id="shopping-list" onSubmit={handleSubmit}> */}
+            <label>
+                Item:
+                    <input type="text" id="name" value={itemName} onChange={(e) => setItemName(e.target.value)} />
+            </label>
+            <br /><br />
+            <label>
+                Quantity:
+                    <input type="number" id="quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+            </label>
+            <select name="quantity-units" id="units" value={quantityUnits} onChange={(e) => setQuantityUnits(e.target.value)}>
+                <option value="kilograms">Kilograms</option>
+                <option value="grams">Grams</option>
+                <option value="litres">Litres</option>
+                <option value="number">Number</option>
+            </select>
+            <button onClick={handleSubmit}>save array</button>
+            <br />
+            <p>Click the submit button to add to shopping list.</p>
+            {/* <input type="submit" value="Submit" /> */}
+            {/* </form> */}
         </div>
     )
 };
