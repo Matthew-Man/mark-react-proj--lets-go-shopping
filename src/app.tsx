2268486a97d74bf6d74024b3a84f7a92175a3adb
Matthew Man <matthew08m@gmail.com>
2021-02-18 12:37:53 +0000

    changes after office hours thursday

--- src/app.tsx
+++ src/app.tsx
@@ -9,6 +9,35 @@ interface ShopListFormData {
 
 // let shoppingList: ShopListFormData[] = [];
 
+function GenerateList() {
+    // console.log("I have re-rendered")
+    // function createListItem(listItem: ShopListFormData, index: number) {
+    //     function removeItem() {
+    //         const copyArray = shoppingList
+    //         const indexPos = copyArray.indexOf(listItem)
+    //         // console.log(indexPos)
+            
+    //         const updatedList = copyArray.splice(indexPos, 1);
+    //         console.log("updated list: ", updatedList)
+
+    //         // const newList = [...shoppingList, updatedList];
+    //         // console.log("shopping list:", shoppingList);
+
+    //         setShoppingList(copyArray);
+    //         console.log("new list: ",copyArray);
+    //     }
+    //     return (
+    //     <div>
+    //         <li key={index}> {listItem.nameOfItem} {listItem.amount} {listItem.amountUnits} <button onClick={removeItem}>x</button> </li>
+    //     </div>
+    //     );
+    // };
+    // return (
+    // <div>
+    //     {shoppingList.map(createListItem)}
+    // </div>
+    // )
+};
 
 function App() {
     
@@ -16,7 +45,8 @@ function App() {
     const [quantity, setQuantity] = useState("0");
     const [quantityUnits, setQuantityUnits] = useState("kilograms");
     //make shopping list with usestate
-    const [shoppingList, setShoppingList] = useState([{}])
+    const emptyBasket: ShopListFormData[] = []
+    const [shoppingList, setShoppingList] = useState(emptyBasket) 
     
 
     function handleSubmit() {
@@ -29,32 +59,35 @@ function App() {
         setQuantityUnits("kilograms")
     }
     
+    function removeItem(itemToRemove: ShopListFormData) {
+        const copyArray = shoppingList
+        const indexPos = copyArray.indexOf(itemToRemove)
+        // console.log(indexPos)
+        
+        const updatedList = copyArray.splice(indexPos, 1);
+        console.log("updated list: ", updatedList)
 
-    function GenerateList() {
-        function createListItem(listItem: any, index: number) {
-            function removeItem() {
-                // const copyArray = shoppingList
-                const indexPos = shoppingList.indexOf(listItem)
-                // console.log(indexPos)
-                
-                const updatedList = shoppingList.splice(indexPos, 1);
-                console.log(updatedList)
-                const newList = [...shoppingList, updatedList];
-                console.log(shoppingList);
-                setShoppingList(newList);
-            }
-            return (
-            <div>
-                <li key={index}> {listItem.nameOfItem} {listItem.amount} {listItem.amountUnits} <button onClick={removeItem}>x</button> </li>
-            </div>
-            );
-        };
+        // const newList = [...shoppingList, updatedList];
+        // console.log("shopping list:", shoppingList);
+
+        setShoppingList(copyArray);
+        console.log("new list: ",copyArray);
+    }
+
+    function createListItem(listItem: ShopListFormData, index: number) {
+        console.log("I am re-rendering the list item")
+        console.log(listItem)
         return (
         <div>
-            {shoppingList.map(createListItem)}
+            <li key={index}> {listItem.nameOfItem} {listItem.amount} {listItem.amountUnits} <button onClick={() => removeItem(listItem)}>x</button> </li>
         </div>
-        )
+        );
     };
+    // return (
+    // <div>
+    //     {shoppingList.map(createListItem)}
+    // </div>
+    // )
     
 
     return (
@@ -78,7 +111,10 @@ function App() {
             <p>Click the submit button to add to shopping list.</p>
             <button onClick={handleSubmit}>submit</button>
             <hr />
-            <GenerateList />
+            <div>
+                {console.log(shoppingList)}
+                {shoppingList.map(createListItem)}
+            </div>
         </div>
     )
 };
